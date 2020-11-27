e3fe0a600b52ba0ca5e5fe8cf10522601295debe
amichalo421 <andre.michalowski@gmail.com>
2020-11-27 13:13:12 -0500

    Day 1: 1-4

--- week1.py
+++ week1.py
@@ -1,1 +1,65 @@
-# Test Test Test
+************************ DAY 1: ************************
+
+# TK NOTES
+
+--------------------------------------------------
+
+1 - INSTALL PYTHON 
+...
+
+--------------------------------------------------
+
+2 - PRINT STATEMENTS:
+
+# Print with an expression
+>>> superlative = "wonderful"
+>>> school = "Lambda School"
+>>> print(school + " is " + superlative)
+Lambda School is wonderful
+>>>
+
+# Print with object types
+print(2020)
+2020
+>>> print(123.456)
+123.456
+>>> print(False)
+False
+>>> print(["Lambda", "School", 2, 0, 2, 0])
+['Lambda', 'School', 2, 0, 2, 0]
+>>> print(("Lambda", "School"))
+('Lambda', 'School')
+>>> print({"school": "Lambda School", "year": 2020})
+{'school': 'Lambda School', 'year': 2020}
+>>>
+
+# End statement
+>>> print("Are you a Lambda School student?", end=" (Y or N)")
+Are you a Lambda School student? (Y or N)>>>
+
+# Sep statement
+>>> print("Hello", "how are you?", sep="---")
+Hello---how are you?
+
+--------------------------------------------------
+
+3 - WHITESPACE
+
+#Whitespace Characters
+Whitespace is any character represented by something that appears empty (usually \t or " "). The characters that Python considers to be whitespace can be seen by printing out the value of string.whitespace from the string library.
+
+>>> import string
+>>> string.whitespace
+' \t\n\r\x0b\x0c'
+>>>
+Notice the characters are " " (space), \t (tab), \n (newline), \r (return), \x0b (unicode line tabulation), and \x0c (unicode form feed).
+
+(REPLIT (CLEAN)) https://repl.it/@mdmccarley89/cs-unit-1-sprint-1-module-1-white-space#main.py
+(REPLIT (DONE)) https://repl.it/@AMichalowski/cs-unit-1-sprint-1-module-1-white-space-1#main.py
+
+
+--------------------------------------------------
+
+4 - BASIC TYPES (INTEGERS, FLOATS, STRINGS)
+
+...
