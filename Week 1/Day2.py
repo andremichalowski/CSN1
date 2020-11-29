ef2b9bc5e9ebd71f08fc6674f6368e03dbf4d972
amichalo421 <andre.michalowski@gmail.com>
2020-11-28 20:04:44 -0500

    Day 2: #1 - 2. Plan

--- Week 1/Day2.py
+++ Week 1/Day2.py
@@ -19,3 +19,22 @@ How performant must the code be?
 Is there anything missing from the task description that needs to be clarified?
 What assumptions are you making?
 Does anyone else on the team need to validate these assumptions?
+
+2. PLAN:
+This step is where you will ask, "what steps will I take to solve the problem?" You will take your description of the problem and transform it into a complete, actionable plan to solve that problem. If you realize you still don't truly understand the problem while planning, return to Understand until you resolve the ambiguity. If you have not yet completed Understand, you will end up planning to solve the wrong problem! When interviewing, you must do this step aloud!
+
+Remember, you aren’t coding during this step unless it’s a small piece of throwaway code to test a hypothesis. It would be best if you wrote pseudocode during this step, however.
+
+Questions
+Do you know the answer to a similar problem that has comparable inputs and outputs?
+Does this problem remind you an anything else?
+Can you bring that knowledge to bear here?
+Does my plan meet the performance requirements?
+What’s the time complexity?
+What’s the space complexity?
+How big can my input data be?
+Can sorting the input data ahead of time lead to any improvements in time complexity?
+Does recursion help?
+Is the problem made up of identical subproblems?
+Can you state the problem with itself in its definition?
+Think like a villain. Does your plan cover the edge cases?
