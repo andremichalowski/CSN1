d7ef84f517c039209843ee78d02c9f13975ba513
amichalo421 <andre.michalowski@gmail.com>
2020-11-28 20:04:06 -0500

    Day 2: #1 - 1. Understand

--- Week 1/Day2.py
+++ Week 1/Day2.py
@@ -3,4 +3,19 @@
 U.P.E.R.
 
 1. UNDERSTAND:
-  - 
+
+Before doing anything else, make sure you understand precisely what the problem is asking. A helpful starting point could be transcribing the technical description of the problem into your own words.
+
+Questions
+Here is a list of starter questions that might come up during this step:
+
+What are the inputs your code receives?
+What is the range of the input?
+How big can the input be (how much data)?
+What are the outputs your code produces?
+What is the range of the output?
+How big can the output be (how much data)?
+How performant must the code be?
+Is there anything missing from the task description that needs to be clarified?
+What assumptions are you making?
+Does anyone else on the team need to validate these assumptions?
