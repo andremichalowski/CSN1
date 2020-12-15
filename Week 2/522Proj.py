9d138af980b181e53f24138be07a34188139d645
amichalo421 <andre.michalowski@gmail.com>
2020-12-14 20:33:45 -0500

    523N

--- Week 2/522Proj.py
+++ Week 2/522Proj.py
@@ -1,1 +1,132 @@
-See codesignal
+1. STRENGTHS OF LINKED LIST:
+
+  1. Fast operations a both ends (head and tail)
+  2. They can grow and shrink to accomodate the data
+
+
+2. WHAT IS THE PRIMARY WEAKNESS OF A LINKED LIST DATA STRUCTURE:
+
+  1. Costly search operations
+
+
+3. ADD A VALUE TO A SORTED LINKED LIST (WHILE RETAINING SORTED):
+
+  def insertValueIntoSortedLinkedList(head, value):
+    new_Node = ListNode(value)
+    # Empty Array
+    if head == None:
+        return new_Node
+    #If value is less than current head replace head
+    if value < head.value:
+        new_Node.next = head
+        return new_Node
+    
+    node = head
+    while node.next != None:
+        if value < node.next.value:
+            break
+        node = node.next
+
+    new_Node.next = node.next
+    node.next = new_Node
+
+    return head
+
+
+4. MERGE TWO SORTED SINGLY LL'S (RETAIN SORT):
+
+  def mergeTwoLinkedLists(head1, head2): 
+  
+    temp = None
+  
+    if head1 is None: 
+        return head2 
+    if head2 is None: 
+        return head1 
+
+    if head1.value <= head2.value: 
+  
+        # assign temp to List1's value 
+        temp = head1 
+  
+        # Again check List1's value is smaller or equal List2's  
+        # value and call mergeLists function. 
+        temp.next = mergeTwoLinkedLists(head1.next, head2) 
+          
+    else: 
+        temp = head2 
+        temp.next = mergeTwoLinkedLists(head1, head2.next) 
+
+    return temp 
+
+
+5. REVERSE NODES IN K-GROUP:
+
+  ListNode* reverseKGroup(ListNode* head, int k) {
+    if (!head || !head->next || k <= 1) {
+        return head;
+    }
+    
+    ListNode dummy(0), *prev = &dummy;
+    prev->next = head;
+    
+    while (true) {
+        ListNode* it = prev->next;
+        int count = 0;
+        while (it) {
+            ++count;
+            if (count == k) {
+                break;
+            }
+            it = it->next;
+        }
+        if (count < k) {
+            break;
+        }
+        
+        ListNode* nextPrev = prev->next;
+        
+        ListNode* prev2 = NULL, *cur = prev->next, *next = NULL;
+        for (int i = 0; i < k; ++i) {
+            next = cur->next;
+            cur->next = prev2;
+            prev2 = cur;
+            cur = next;
+        }
+        
+        prev->next = prev2;
+        nextPrev->next = cur;
+        
+        prev = nextPrev;
+    }
+    
+    return dummy.next;
+  }
+
+
+  ---
+
+
+  function reverseNodesInKGroups(l, k) {
+      return calculate(l, k , Math.floor(sizeOf(l)/k))
+  }
+  const calculate = (l, k, numberOfInteractions) => {
+      if(numberOfInteractions===0) { return l }
+      let first = l
+      let previous = l
+      let current = l
+      let lostLink = current.next 
+      for(let i = 0; i < k-1; i++){
+          current = lostLink
+          lostLink = current.next
+          current.next = previous
+          previous = current
+      }
+      first.next = calculate(lostLink, k, numberOfInteractions-1)
+      return current
+  }
+  const sizeOf = (l) => {
+      let size = 0
+      while(l){ size++; l = l.next }
+      return size
+  }
--- Week 2/523.py
+++ Week 2/523.py
@@ -1,1 +1,141 @@
-test
+1. "QUEUE"
+
+  1. FIFO
+
+  2. Space + Time Complexity:
+    - Space = O(n)
+    - Time = O(1)
+
+    Enqueue - add item to back 
+    Dequeue - remove item from front
+    Peek - inspect item from front of queue
+
+  3. Strengths + Weaknesses:
+    - S: O(1)
+    - W: None
+    
+
+2. "STACK"
+
+  1. LIFO
+
+  2. Space + Time Complexity:
+    - Space = 0(n)
+    - Time = O(1)
+
+    Push - add to top
+    Pop - remove from top
+    Peek - inspect top
+
+  3. Strengths + Weaknesses:
+    - S: O(1)
+    - W: None (not very versatile though...?)
+
+
+3. IMPLEMENT A QUE USING A LL:
+
+   1. Create LLN:
+      class LinkedListNode:
+        def __init__(self, data):
+          self.data = data
+          self.next = None
+
+
+    2. Create QUE:
+
+      class Queue:
+        def __init__(self):
+          self.front = None
+          self.rear = None
+
+
+    3. Define enqueue method:
+
+        def enqueue(self, item):
+          new_node = LinkedListNode(item)
+          # check if que is empty
+          if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+          else: 
+            # add new node to rear
+            self.rear.next = new_node
+            # reassign rear to new node
+            self.rear = new_node
+
+    4. Define dequeue method:
+
+
+        def dequeue(self):
+          # check if queue is empty
+          if self.front is not None:
+            # keep copy of old front
+            old_front = self.front 
+            self.front = old_front.next
+
+          # check if que now epty
+          if self.front is None:
+            self.rear = None
+
+          return old_front
+
+
+4. IMPLEMENT A STACK USING A DYNAMIC ARRAY:
+
+  1. Define stack class and __init__ method:
+
+    class Stack:
+      def __init__(self):
+        self.data = []
+
+  2. Define a push method to add an item to the top of stack:
+
+      def push(self, item):
+          self.data.append(item)
+
+  3. Define a pop method to remove an item from the top of stack:
+
+      def pop(self, item):
+          if len(self.data) > 0:
+            return self.data.pop()
+          return "The stack is empty"
+
+
+5. IMPLEMENT A STACK USING A LINKED LIST:
+
+  1. Define stack and __init__ method:
+
+    class LinkedListNode:
+      def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    class Stack:
+      def __init__(self):
+        self.top = None
+
+  2. Define push method to add to top of stack:
+
+      def push(self, data):
+        # create new node with data
+        new_node = LinkedListNode(data)
+        # set current top to new node's next
+        new_node.next = self.top
+        # reset the top pointer to the new node
+        self.top = new_node
+
+  3. Define a pop method to add to bottom
+
+      def pop(self):
+        # make sure stack is not empty
+        if self.top is not None:
+          # store popped node
+          popped_node = self.top
+          # reset top pointer to next node
+          self.top = popped_node.next
+          # return the value from the popped node
+          return popped_node.data
+
+
+    
+
