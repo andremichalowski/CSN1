571de65d81147c0dd6a0358e33146a32cf36fa39
amichalo421 <andre.michalowski@gmail.com>
2020-12-24 07:55:28 -0500

    524L2

--- Week 2/524.py
+++ Week 2/524.py
@@ -30,3 +30,26 @@ def sum_list(items):
     for i in items:
         sum = sum + i
     return sum
+
+
+Let's start writing out our function in pseudocode:
+
+sum_list(items)
+    if the length of items is one
+        return the one item in the list
+But what if someone asks us to sum more than one item? We must sum the first number and the sum of the rest of the items in the list.
+
+Let's add this to our pseudocode:
+
+sum_list(items)
+    if the length of items is one
+        return the one item in the list
+    otherwise
+        return the first item from the list + sum_list(the rest of the items)
+Now let's convert our pseudocode into actual Python code:
+
+def sum_list(items):
+    if len(items) == 1:
+        return items[0]
+    else:
+        return items[0] + sum_list(items[1:])
