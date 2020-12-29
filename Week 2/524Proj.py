cbc3169a53131537311d9cbef39058986681ccdd
amichalo421 <andre.michalowski@gmail.com>
2020-12-29 12:22:10 -0500

    524P5

--- Week 2/524Proj.py
+++ Week 2/524Proj.py
@@ -37,3 +37,33 @@
     return False
 
     What must be true for this algorithm to work?
+
+
+5. FIBONACCI SIMPLE SUM:
+
+  For a given positive integer n determine if it can be represented as a sum of two Fibonacci numbers (possibly equal).
+
+  def fibonacciSimpleSum2(number):
+    if number < 12:
+        return True
+
+    numbers = {0, 1, 2, 3, 5}
+
+    last_number = 5
+    this_number = 8
+
+    while this_number <= number:
+
+        numbers.add(this_number)
+        target = number - this_number
+
+        if target in numbers or target == this_number:
+            return True
+        
+        next_number = last_number + this_number
+        last_number = this_number
+        this_number = next_number
+    return False
+
+
+6. 
