9525312645b095d49942e7171ed3abfcc899caad
amichalo421 <andre.michalowski@gmail.com>
2020-12-29 14:29:13 -0500

    531N2-3

--- Week 3/531.py
+++ Week 3/531.py
@@ -41,3 +41,47 @@
       1. Unbalanced trees are more inefficient 
       2. Not especially efficient in anything Specific #(Even though good at general purpose efficiency)
 
+3. CONSTRUCT A BINARY SEARCH TREE THAT CAN PERFORM BASIC OPERATIONS WITH A LOGARITHMIC TIME COMPLEXITY:
+  1. Node class:
+    class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def search(self, target):
+        if self.value == target:
+            return self
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.search(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(target)
+
+  2. BST CLASS:
+    class BST:
+      def __init__(self, value):
+          self.root = BSTNode(value)
+
+      def insert(self, value):
+          self.root.insert(value)
+
+      def search(self, value):
+          self.root.search(value)
