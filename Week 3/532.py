047f737fb28597609e3b85250f2ac8c7180cee63
amichalo421 <andre.michalowski@gmail.com>
2020-12-29 20:36:13 -0500

    532N-2 Breadth First (PreOrder)

--- Week 3/532.py
+++ Week 3/532.py
@@ -32,3 +32,27 @@
         return result
 
 
+  2. DEPTH FIRST (PREORDER):
+
+    Visit the node
+    Go to the left subtree
+    Go to the right subtree
+
+    class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+    def helper(root, res):
+        if root is None:
+            return
+        res.append(root.val)
+        helper(root.left, res)
+        helper(root.right, res)
+
+    def preorder_traversal(root):
+        result = []
+        helper(root, result)
+        return result
+
