bfba9c9938ae13400c5ff27a8864a3536233aa8e
amichalo421 <andre.michalowski@gmail.com>
2021-01-01 11:47:11 -0500

    533N: 2 (Adjacency List + Matrix)

---
+++ Week 3/533.py
@@ -0,0 +1,31 @@
+
+1. WHAT IS A GRAPH, ITS COMPONENTS, AND USEFULL APPLICATIONS:
+
+  https://lambdaschool.instructure.com/courses/582/pages/objective-01-describe-what-a-graph-is-explain-its-components-provide-examples-of-its-useful-applications-and-draw-each-of-the-different-graph-types?module_item_id=527099
+
+  A. TYPES:
+
+    1. DIRECTED AND UNDIRECTED:
+      A. Directed:
+        - uni-directional
+        - bi-directional
+      B. Undirected:
+        - no-direction
+
+    2. CYCLIC AND ACYCLIC:
+      A. Cyclic:
+        - able to re-visit a vert #undirected is cyclic
+      B. Acyclic:
+        - not able to revisit verts 
+
+    3. WEIGHTED:
+      A. Weighted:
+        - some edges have different weights
+
+    4. DIRECTED ACYCLIC GRAPHS (DAG'S):
+
+
+
+2. ADJACENCY LIST + ADJACENCY MATRIX:
+
+    Graph Representations
