42eb61045fd5319a51a42bb46e80748adce783aa
amichalo421 <andre.michalowski@gmail.com>
2021-01-03 11:25:20 -0500

    533N2: Adj L + M Comp

--- Week 3/533.py
+++ Week 3/533.py
@@ -28,7 +28,76 @@
 
 2. ADJACENCY LIST + ADJACENCY MATRIX:
 
-    Graph Representations
+    A. GRAPH REPRESENTATIONS:
+      1. Adjacency GRAPH
+      2. Adjacency LIST
+      3. Adjacency MATRIX
+
+
+      1. ADJACENCY GRAPH:
+        - Visual representation of vertices using shapes and arrows (see link)
+
+      2, ADJACENCY LIST:
+        - Dictionary that holds a list of vertices connected verteces. 
+        class Graph:
+          def __init__(self):
+              self.vertices = {
+                                  "A": {"B"},
+                                  "B": {"C", "D"},
+                                  "C": {"E"},
+                                  "D": {"F", "G"},
+                                  "E": {"C"},
+                                  "F": {"C"},
+                                  "G": {"A", "F"}
+                              }
+
+      3. ADJACENCY MATRIX:
+        - Two dimensional array, a list of lists.
+        class Graph:
+          def __init__(self):
+              self.edges = [[0,1,0,0,0,0,0],
+                            [0,0,1,1,0,0,0],
+                            [0,0,0,0,1,0,0],
+                            [0,0,0,0,0,1,1],
+                            [0,0,1,0,0,0,0],
+                            [0,0,1,0,0,0,0],
+                            [1,0,0,0,0,1,0]]
+
+
+    B. TRADEOFFS:
+
+      Shorthand	Property
+        V	Total number of vertices in the graph
+        E	Total number of edges in the graph
+        e	Average number of edges per vertex
+
+      
+      Type	  Space	  Add-Vert	Remove-Vert	 Add-Edge	 Remove-Edge	Find-Edge	  Get-All-Edges
+      List	  O(V+E)	O(1)	    O(V)	       O(1)	     O(1)	        O(1)	      O(1)
+      Matrix	O(V^2)	O(V)	    O(V^2)	     O(1)	     O(1)	        O(1)	      O(V)
+
+    
+    C. WEIGHTS EXAMPLES:
+
+      1. LIST:
+        class Graph:
+          def __init__(self):
+              self.vertices = {
+                                  "A": {"B": 1},
+                                  "B": {"C": 3, "D": 2},
+                                  "C": {},
+                                  "D": {},
+                                  "E": {"D": 1}
+                              }
+
+      2. MATRIX:
+        class Graph:
+          def __init__(self):
+              self.edges = [[0,1,0,0,0],
+                            [0,0,3,2,0],
+                            [0,0,0,0,0],
+                            [0,0,0,0,0],
+                            [0,0,0,1,0]]
 
 
 3. IMPLEMENT USER DEFINED VERTEX AND GRAPH CLASSES THAT ALLOW BASIC OPERATIONS:
