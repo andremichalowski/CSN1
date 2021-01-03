50f40878bfcaf5b40870847b10c72adcb3bd7997
amichalo421 <andre.michalowski@gmail.com>
2021-01-03 16:04:49 -0500

    533L2: 2 - Legal Graph Coloring

--- Week 3/533L.py
+++ Week 3/533L.py
@@ -34,3 +34,62 @@ Repo: https://github.com/LambdaSchool/cs-guided-project-graphs-i
   Collapse
 
 
+2. LEGAL GRAPH COLORING:
+
+  class GraphNode:
+      def __init__(self, val):
+          self.val = val
+          self.neighbors = set()
+          self.color = None
+  ​
+  colors = ['Red', 'Green', 'Yellow', 'Blue', 'Purple']
+  ​
+  def color_graph(graph, colors):
+      for node in graph: 
+
+          illegal_colors = set() # Set illegal colors and add them respectively based on neighbor colors
+          for neighbor in node.neighbors: 
+              if neighbor.color not in illegal_colors:
+                  illegal_colors.add(neighbor.color)
+  ​
+          for color in colors: #Whatever color from color list that is not illegal add
+              if color not in illegal_colors:
+                  node.color = color 
+                  break
+  ​
+  ​
+  g1 = GraphNode('G1')
+  g2 = GraphNode('G2')
+  g3 = GraphNode('G3')
+  g4 = GraphNode('G4')
+  g5 = GraphNode('G5')
+  ​
+  g1.neighbors.add(g2)
+  g1.neighbors.add(g4)
+  g1.neighbors.add(g3)
+  ​
+  g2.neighbors.add(g1)
+  g2.neighbors.add(g4)
+  g2.neighbors.add(g5)
+  ​
+  g3.neighbors.add(g1)
+  g3.neighbors.add(g5)
+  g3.neighbors.add(g4)
+  ​
+  g4.neighbors.add(g1)
+  g4.neighbors.add(g2)
+  g4.neighbors.add(g3)
+  g4.neighbors.add(g5)
+  ​
+  g5.neighbors.add(g2)
+  g5.neighbors.add(g3)
+  g5.neighbors.add(g4)
+  ​
+  graph = [g1, g2, g3, g4, g5]
+  ​
+  color_graph(graph, colors)
+  ​
+  for node in graph:
+      print(node.color)
+
+
