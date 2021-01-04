641a65b99da5f08b3d35bb54f4393f3dcae6cbe5
amichalo421 <andre.michalowski@gmail.com>
2021-01-04 18:53:47 -0500

    541N: 1 - Inrto Hash

---
+++ Week 4/541.py
@@ -0,0 +1,13 @@
+1. HASH TABLES INTRO:
+
+  TC: (BEST CASE/WORST CASE) = O(1) / O(n)
+  SC: O(n)
+
+  Strengths: 
+    O(1) average lookup etc. 
+    Can use any hashable object as key
+
+  Weakness:
+    mapping goes one way so unless you know the key its going to be innefficient to find the value
+
+
