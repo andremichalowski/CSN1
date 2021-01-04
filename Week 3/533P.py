765cbfb928e3b62af1bb4711e57bc0faaa4b4c68
amichalo421 <andre.michalowski@gmail.com>
2021-01-03 19:56:11 -0500

    533P

--- Week 3/533P.py
+++ Week 3/533P.py
@@ -1,31 +1,51 @@
 
-1. WHAT IS A GRAPH, ITS COMPONENTS, AND USEFULL APPLICATIONS:
-
-  https://lambdaschool.instructure.com/courses/582/pages/objective-01-describe-what-a-graph-is-explain-its-components-provide-examples-of-its-useful-applications-and-draw-each-of-the-different-graph-types?module_item_id=527099
-
-  A. TYPES:
-
-    1. DIRECTED AND UNDIRECTED:
-      A. Directed:
-        - uni-directional
-        - bi-directional
-      B. Undirected:
-        - no-direction
-
-    2. CYCLIC AND ACYCLIC:
-      A. Cyclic:
-        - able to re-visit a vert #undirected is cyclic
-      B. Acyclic:
-        - not able to revisit verts 
-
-    3. WEIGHTED:
-      A. Weighted:
-        - some edges have different weights
-
-    4. DIRECTED ACYCLIC GRAPHS (DAG'S):
+1. GRAPHS AND TREES RELATIONSHIPS:
+  - Which of the following statements is true?
+   >>>> All trees are graphs but not all graphs are trees.
 
 
+2. DIRECTED VERSUS UNDIRECTED GRAPHS:
+  - Choose answer that best describes difference between directed and undirected graph. 
+  >>>> Directed graphs best represent relationships that can be described as "mutual exchanges", while undirected graphs best represent relationships that can be described as, "one way".
 
-2. ADJACENCY LIST + ADJACENCY MATRIX:
+3. GRAPH ADJACENCY LIST REPRESENTATION:
+  - Choose the python code that correctly represents the graph shown in the picture using an adjacency list. 
+  >>>>> (An adjacency list that accurately describes the picture... (See Screenshot))
 
-  
+4. GRAPH REPRESENTATION IN PYTHON INTERPRETER:
+  - select python code written in an interactive interpreter that would correctly create the graph pictured)
+  >>>>> (See screenshot)
+
+5. FIND ALL PATHS FROM A TO B:
+  - You are given a directed acyclic graph (DAG) that contains N nodes.
+  Write a fxn that can find all the possible paths from node 0 to node N-1. You can return the path in any order.
+  graph[a] is a list of all nodes b for which the edge a -> b exists.
+
+  # directed_graph = [
+  #     [1, 2],
+  #     [3],
+  #     [3],
+  #     [4],
+  #     []
+  # ]
+
+
+  def csFindAllPathsFromAToB( graph ):
+      class GraphNode:
+          def __init__(self, value=None):
+              self.value = value
+              self.connections = []
+          
+      paths= []
+      currentPath= [0]
+      targetNode= len(graph)-1
+      def helper(currentPath, index):
+          if index==targetNode:
+              paths.append(currentPath.copy())
+              return
+          for i in range(0, len(graph[index])):
+              currentPath.append(graph[index][i])
+              helper(currentPath, graph[index][i])
+              currentPath.pop()
+      helper(currentPath, 0)
+      return paths
