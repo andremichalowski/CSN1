2387901a5c40e088d1badffce3b00d70ccfd23b2
amichalo421 <andre.michalowski@gmail.com>
2021-01-04 18:54:08 -0500

    541N: 2 - Implement HF

--- Week 4/541.py
+++ Week 4/541.py
@@ -11,3 +11,16 @@
     mapping goes one way so unless you know the key its going to be innefficient to find the value
 
 
+2. DESCRIBE AND IMPLEMENT A HASH FUNCTION:
+
+  def my_hashing_func(str, table_size):
+    bytes_representation = str.encode()
+
+    sum = 0
+    for byte in bytes_representation:
+        sum += byte
+
+    return sum % table_size
+
+
+3. 
