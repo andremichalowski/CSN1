739b0226700ab2c0fa70ce1ac706534d72175d60
amichalo421 <andre.michalowski@gmail.com>
2021-01-08 16:01:20 -0500

    542P

---
+++ Week 4/542P.py
@@ -0,0 +1,43 @@
+1. 
+
+2. 
+
+3. ISOMETRIC STRINGS:
+
+  Given two strings a and b, determine if they are isomorphic.
+
+  Two strings are isomorphic if the characters in a can be replaced to get b.
+
+  All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+
+  def csIsomorphicStrings(a, b):
+      return [a.find(i) for i in a] == [b.find(j) for j in b]
+
+4. WORD PATTERN:
+
+  Given a pattern and a string a, find if a follows the same pattern.
+
+  Here, to "follow" means a full match, such that there is a one-to-one correspondence between a letter in pattern and a non-empty word in s.
+
+  def csWordPattern(pattern, a):
+      s = pattern
+      t = a.split()
+      return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
+
+  
+5. GROUP ANAGRAMS:
+
+Given an array of strings strs, write a function that can group the anagrams. The groups should be ordered such that the larger groups come first, with subsequent groups ordered in descending order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different wo
+
+  def csGroupAnagrams(strs):
+      anas = {}
+      for string in strs:
+          s = ''.join(sorted(string))
+          if s in anas:
+              anas[s].append(string)
+          else:
+              anas[s] = [string]
+      return [ anas[x] for x in anas ]
