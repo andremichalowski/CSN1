c444e45fdd07450dde1c809cf518dbcae467eff7
amichalo421 <andre.michalowski@gmail.com>
2021-01-11 18:42:54 -0500

    DLC: 7 - Intersection of Two Arrays (3ex's)

--- Daily Leetcode/DLC.py
+++ Daily Leetcode/DLC.py
@@ -82,4 +82,61 @@
 
 (Daily Leetcode January 9th)
 
+Two pointers:
+    class Solution(object):
+        def intersect(self, nums1, nums2):
+
+            nums1, nums2 = sorted(nums1), sorted(nums2)
+            pt1 = pt2 = 0
+            res = []
+
+            while True:
+                try:
+                    if nums1[pt1] > nums2[pt2]:
+                        pt2 += 1
+                    elif nums1[pt1] < nums2[pt2]:
+                        pt1 += 1
+                    else:
+                        res.append(nums1[pt1])
+                        pt1 += 1
+                        pt2 += 1
+                except IndexError:
+                    break
+
+            return res
+
+
+Use dictionary to count:
+    class Solution(object):
+        def intersect(self, nums1, nums2):
+
+            counts = {}
+            res = []
+
+            for num in nums1:
+                counts[num] = counts.get(num, 0) + 1
+
+            for num in nums2:
+                if num in counts and counts[num] > 0:
+                    res.append(num)
+                    counts[num] -= 1
+
+            return res
+
+
+Use Counter to make it cleaner:
+    class Solution(object):
+        def intersect(self, nums1, nums2):
+
+            counts = collections.Counter(nums1)
+            res = []
+
+            for num in nums2:
+                if counts[num] > 0:
+                    res += num,
+                    counts[num] -= 1
+
+            return res
+
+
 8. MOVE ZEROES:
