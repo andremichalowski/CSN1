c26f99d952b162249f93d5cc5113adab73bf8acf
amichalo421 <andre.michalowski@gmail.com>
2021-01-19 11:07:54 -0500

    DLC Strings: 2 - Reverse int w/signs

--- Daily Leetcode/DLC_Strings.py
+++ Daily Leetcode/DLC_Strings.py
@@ -3,3 +3,19 @@
   for i in range(len(s)//2):
     s[i], s[-i-1] = s[-i-1], s[i]
 
+
+2. REVERSE INT W/SIGNS:
+  class Solution:
+    def reverse(self, x: int) -> int:
+        result = 0
+        symbol = 1
+        
+        if x < 0:
+            symbol = -1
+            x = -x
+
+        while x:
+            result = result * 10 + x % 10
+            x //= 10
+            
+        return 0 if result > pow(2,31) else result * symbol
