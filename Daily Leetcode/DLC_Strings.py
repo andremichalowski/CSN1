a70748adf46edf6e8624f3dab1300bc3b09aa65c
amichalo421 <andre.michalowski@gmail.com>
2021-01-24 16:41:39 -0500

    DLC Strings: 8 - See and say

--- Daily Leetcode/DLC_Strings.py
+++ Daily Leetcode/DLC_Strings.py
@@ -47,4 +47,37 @@
     s = ''.join(e for e in s if e.isalnum()).lower()
     return s==s[::-1]
 
-    
+
+6. STRING TO INTEGER: (ATOI):
+
+7. IMPLEMENT strStr():
+
+8. SEE AND SAY: (How many repeats of each num):
+
+  def countAndSay(self, n):
+      # The first char of our result is 1 so initilize the result
+      s = '1'
+      
+      # As we have taken care of case 1 with n == 1 we start from 2
+      while n > 1:
+          
+          i = 0
+          # create new string every iteration and assign it to our res
+          ns = ''
+          while i < len(s):
+              # As we already included the first character so count = 1
+              count = 1
+              # Check if the prev char is same as next and keep count
+              while i+1 < len(s) and s[i] == s[i+1]:
+                  i += 1
+                  count += 1
+              # Finally make the new string
+              ns += str(count) + s[i]
+              i += 1
+          # Assign the newly made string for next iteration to s
+          s = ns
+          n -= 1
+      return s
+
+
+return countAndSay(34556)
--- Daily Leetcode/Test.py
+++ Daily Leetcode/Test.py
@@ -1,16 +1,44 @@
-nums = [0, 1, 2, 0, 3]
+# nums = [0, 1, 2, 0, 3]
 
-def moveZeroes(nums):
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    for i in range(len(nums)):
-        if nums[i] == 0:
-            nums.remove(nums[i])
-            nums.append(0)
-            print(nums)
-        else:
-            continue
-        return nums
+# def moveZeroes(nums):
+#     """
+#     Do not return anything, modify nums in-place instead.
+#     """
+#     for i in range(len(nums)):
+#         if nums[i] == 0:
+#             nums.remove(nums[i])
+#             nums.append(0)
+#             print(nums)
+#         else:
+#             continue
+#         return nums
 
-moveZeroes(nums)
+# moveZeroes(nums)
+
+def countAndSay(n):
+    # The first char of our result is 1 so initilize the result
+    s = '1'
+    
+    # As we have taken care of case 1 with n == 1 we start from 2
+    while n > 1:
+        
+        i = 0
+        # create new string every iteration and assign it to our res
+        ns = ''
+        while i < len(s):
+            # As we already included the first character so count = 1
+            count = 1
+            # Check if the prev char is same as next and keep count
+            while i+1 < len(s) and s[i] == s[i+1]:
+                i += 1
+                count += 1
+            # Finally make the new string
+            ns += str(count) + s[i]
+            i += 1
+        # Assign the newly made string for next iteration to s
+        s = ns
+        n -= 1
+    return s
+
+
+print(countAndSay(34556))
