83035a8ec97b0d6d000a96046d653927878333cc
amichalo421 <andre.michalowski@gmail.com>
2021-02-17 11:16:11 -0500

    DLC_Lists: 4 - Merge two sorted lists (recursive)

--- Daily Leetcode/DLC_3_Lists.py
+++ Daily Leetcode/DLC_3_Lists.py
@@ -93,4 +93,13 @@
     temp.next = l1 or l2
     return dummy.next
 
-
+  def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+      if not l1 or not l2:
+          return l1 or l2
+      
+      if l1.val <= l2.val: #1
+          l1.next = self.mergeTwoLists(l1.next, l2)
+          return l1
+      else: #2
+          l2.next = self.mergeTwoLists(l1, l2.next)
+          return l2
