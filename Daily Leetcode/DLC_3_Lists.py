0248460c80431a24a4a91815be547f09dec5caad
amichalo421 <andre.michalowski@gmail.com>
2021-02-17 10:47:27 -0500

    DLC_Lists: 4 - Merge two sorted lists

--- Daily Leetcode/DLC_3_Lists.py
+++ Daily Leetcode/DLC_3_Lists.py
@@ -78,4 +78,19 @@
           return new_l
 
 
+4. MERGE TWO SORTED LISTS:
+
+  def mergeTwoLists(self, l1, l2):
+    dummy = temp = ListNode(0)
+    while l1 and l2:
+      if l1.val < l2.val:
+        temp.next = l1
+        l1 = l1.next
+      else:
+        temp.next = l2
+        l2 = l2.next
+      temp = temp.next
+    temp.next = l1 or l2
+    return dummy.next
+
 
