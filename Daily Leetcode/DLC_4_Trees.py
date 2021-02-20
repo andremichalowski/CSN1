50a91185039b786282c0e0a1ad8c79eb13149cd3
amichalo421 <andre.michalowski@gmail.com>
2021-02-20 12:28:20 -0500

    DLC Trees: 2 - Is Valid BST

--- Daily Leetcode/DLC_4_Trees.py
+++ Daily Leetcode/DLC_4_Trees.py
@@ -5,3 +5,20 @@
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+2. IS VALID BST:
+
+  def isValidBST(self, root: TreeNode) -> bool:
+    def inorder(root):
+                if not root:
+                    return True
+                if not inorder(root.left):
+                    return False
+                if root.val <= self.prev:
+                    return False
+                self.prev = root.val
+                return inorder(root.right)
+
+            self.prev = -math.inf
+            return inorder(root)
