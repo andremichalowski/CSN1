d82aaadb5be9938934a41d57b6b1ff15914d08a6
amichalo421 <andre.michalowski@gmail.com>
2021-02-20 12:32:01 -0500

    DLC Trees: 2 - Is Valid BST

--- Daily Leetcode/DLC_4_Trees.py
+++ Daily Leetcode/DLC_4_Trees.py
@@ -9,16 +9,35 @@
 
 2. IS VALID BST:
 
-  def isValidBST(self, root: TreeNode) -> bool:
-    def inorder(root):
-                if not root:
-                    return True
-                if not inorder(root.left):
-                    return False
-                if root.val <= self.prev:
-                    return False
-                self.prev = root.val
-                return inorder(root.right)
+https://www.youtube.com/watch?v=MILxfAbIhrE
 
-            self.prev = -math.inf
-            return inorder(root)
+  class Node: 
+    
+      # Constructor to create a new node 
+      def __init__(self, data): 
+          self.data = data  
+          self.left = None
+          self.right = None
+    
+    
+  # Returns true if the given tree is a binary search tree 
+  # (efficient version) 
+  def isBST(node): 
+      return (isBSTUtil(node, INT_MIN, INT_MAX)) 
+    
+  # Retusn true if the given tree is a BST and its values 
+  # >= min and <= max 
+  def isBSTUtil(node, mini, maxi): 
+        
+      # An empty tree is BST 
+      if node is None: 
+          return True
+    
+      # False if this node violates min/max constraint 
+      if node.data < mini or node.data > maxi: 
+          return False
+    
+      # Otherwise check the subtrees recursively 
+      # tightening the min or max constraint 
+      return (isBSTUtil(node.left, mini, node.data -1) and
+            isBSTUtil(node.right, node.data+1, maxi)) 
