57971529e8172c226f0e210c71b2d9af30d636fe
amichalo421 <andre.michalowski@gmail.com>
2021-03-02 11:20:01 -0500

    DLC Trees: 5 - Convert sorted array to BST

--- Daily Leetcode/DLC_4_Trees.py
+++ Daily Leetcode/DLC_4_Trees.py
@@ -100,3 +100,17 @@ def levelOrder(self, root):
                 temp.extend([node.left, node.right])
             level = [leaf for leaf in temp if leaf]
         return ans
+
+5. CONVERT SORTED ARRAY TO BINARY SEARCH TREE:
+
+     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return root
