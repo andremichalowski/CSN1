025e4da2409a099491945640108a0c25795e5bc1
amichalo421 <andre.michalowski@gmail.com>
2021-03-09 15:34:42 -0500

    DLC Sorting: 2 - First Bad Version

--- Daily Leetcode/DLC_5_Sorting-and-Searching.py
+++ Daily Leetcode/DLC_5_Sorting-and-Searching.py
@@ -4,4 +4,15 @@
           nums1[m:] = nums2[:n]
           nums1.sort()
 
-2. 
+2. FIRST BAD VERSION:
+
+  def firstBadVersion(self, n):
+        low = 1
+        high = n
+        while(low < high):
+            mid = (low + high)/2
+            if isBadVersion(mid) == True:
+                high = mid
+            else:
+                low = mid + 1
+        return int(high)
