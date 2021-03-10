b04a34b6fb58e7934b0ac0e101c7488a7a95118f
amichalo421 <andre.michalowski@gmail.com>
2021-03-10 12:13:43 -0500

    DLC  Sorting: 2 - First Bad Version (Recursive)

--- Daily Leetcode/DLC_5_Sorting-and-Searching.py
+++ Daily Leetcode/DLC_5_Sorting-and-Searching.py
@@ -6,6 +6,7 @@
 
 2. FIRST BAD VERSION:
 
+Iterative Version:
   def firstBadVersion(self, n):
         low = 1
         high = n
@@ -16,3 +17,16 @@
             else:
                 low = mid + 1
         return int(high)
+
+Recursive Version:
+  def firstBadVersion(self, n):
+      return self.binarySearch(1, n)
+  
+  def binarySearch(self, low, high):
+      if low == high:
+          return low
+      mid = (low + high)/2
+      if isBadVersion(mid) == True:
+          return self.binarySearch(low, mid)
+      else:
+          return self.binarySearch(mid + 1, high)
