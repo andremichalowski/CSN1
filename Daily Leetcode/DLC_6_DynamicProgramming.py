42771eb0035e853035029fd2b1751d6fe6a2bd57
amichalo421 <andre.michalowski@gmail.com>
2021-03-12 09:37:45 -0500

    DLC DP: 3 - Maximum sub array

--- Daily Leetcode/DLC_6_DynamicProgramming.py
+++ Daily Leetcode/DLC_6_DynamicProgramming.py
@@ -23,3 +23,12 @@
       maxProfit = max(maxProfit, prices[i] - minPurchase)
       minPurchase = min(minPurchase, prices[i])
     return maxProfit
+
+
+3. MAXIMUM SUB ARRAY: (KADANES ALGORITHM):
+
+  for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] += nums[i-1]
+    return max(nums)
+
