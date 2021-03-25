5b06051d25c7354a055a3f148243c44965daf9e0
amichalo421 <andre.michalowski@gmail.com>
2021-03-25 15:51:33 -0400

    DLC Math: 1 - Fizz Buzz (Naive)

---
+++ Daily Leetcode/DLC_8_Math.py
@@ -0,0 +1,18 @@
+1. FIZZ BUZZ:
+
+# n = [ "1", "2", "3", "4", "5" ]
+
+# def testFizz(n):
+#   for num in range(1, (int(n))+1):
+#     return num
+
+# print(testFizz(n))
+
+# Naive Approach
+
+
+
+
+
+
+
