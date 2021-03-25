8abc6c64638380d2f21c3dc8bbc242a2fb5a9f29
amichalo421 <andre.michalowski@gmail.com>
2021-03-25 15:54:16 -0400

    DLC Math: 1 - Fizz Buzz (String concat)

--- Daily Leetcode/DLC_8_Math.py
+++ Daily Leetcode/DLC_8_Math.py
@@ -9,8 +9,68 @@
 # print(testFizz(n))
 
 # Naive Approach
+  class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        # ans list
+        ans = []
+
+        for num in range(1,n+1):
+
+            divisible_by_3 = (num % 3 == 0)
+            divisible_by_5 = (num % 5 == 0)
+
+            if divisible_by_3 and divisible_by_5:
+                # Divides by both 3 and 5, add FizzBuzz
+                ans.append("FizzBuzz")
+            elif divisible_by_3:
+                # Divides by 3, add Fizz
+                ans.append("Fizz")
+            elif divisible_by_5:
+                # Divides by 5, add Buzz
+                ans.append("Buzz")
+            else:
+                # Not divisible by 3 or 5, add the number
+                ans.append(str(num))
+
+        return ans
 
 
+# String concatonation
+
+  class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        # ans list
+        ans = []
+
+        for num in range(1,n+1):
+
+            divisible_by_3 = (num % 3 == 0)
+            divisible_by_5 = (num % 5 == 0)
+
+            num_ans_str = ""
+
+            if divisible_by_3:
+                # Divides by 3
+                num_ans_str += "Fizz"
+            if divisible_by_5:
+                # Divides by 5
+                num_ans_str += "Buzz"
+            if not num_ans_str:
+                # Not divisible by 3 or 5
+                num_ans_str = str(num)
+
+            # Append the current answer str to the ans list
+            ans.append(num_ans_str)  
+
+        return ans
 
 
 
