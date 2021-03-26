e8f88ceb3b0b530cb191fedd7f4a6f4dd1b8a098
amichalo421 <andre.michalowski@gmail.com>
2021-03-26 12:22:51 -0400

    DLC - Math: 2 - Count Primes

--- Daily Leetcode/DLC_8_Math.py
+++ Daily Leetcode/DLC_8_Math.py
@@ -106,5 +106,19 @@
         return ans
 
 
+2. COUNT PRIMES:
+
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        dp = [True] * n
+        dp[0] = dp[1] = False
+        for i in range(2, n):
+            if dp[i]:
+                for j in range(i*i, n, i):
+                    dp[j] = False
+        return sum(dp)
+
+
 
 
