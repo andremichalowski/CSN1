cdf4080ef0689202efd83773648785632232473f
amichalo421 <andre.michalowski@gmail.com>
2021-05-02 16:57:27 -0400

    DLC 8 Math: 4 - Roman to Integer

--- Daily Leetcode/DLC_8_Math.py
+++ Daily Leetcode/DLC_8_Math.py
@@ -180,3 +180,16 @@ def isPowerOfThree(self, n: int) -> bool:
         if n % 3 > 0: return False
         n = n / 3
     return n == 1
+
+4. ROMAN TO INTEGER:
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/102/math/878/
+
+def romanToInteger(self, n):
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    z = 0
+    for i in range(0, len(s) - 1):
+        if roman[s[i]] < roman[s[i+1]]:
+            z -= roman[s[i]]
+        else:
+            z += roman[s[i]]
+    return z + roman[s[-1]]
