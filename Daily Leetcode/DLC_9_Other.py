a8b1766cd16b0f35bc81eafbd2742b9f5b98559a
amichalo421 <andre.michalowski@gmail.com>
2021-05-14 08:40:36 -0400

    DLC 9 Other: HD cnt. (XOR Explanation Link)

--- Daily Leetcode/DLC_9_Other.py
+++ Daily Leetcode/DLC_9_Other.py
@@ -14,42 +14,43 @@
         c += 1
     return c
 
-    2. Hamming Distance:
+2. Hamming Distance:
 
-      # Easy Way
-      bin(x ^ y).count('1')
+    # Easy Way
+    bin(x ^ y).count('1')
 
-      #Right way (Bitwise Operators): https://code.tutsplus.com/articles/understanding-bitwise-operators--active-11301
-      
-      # Approach 1: Just check every bit in both numbers and increment when they are different
-      def hammingDistance(self, x: int, y: int) -> int:
-          hamming_distance = 0
-          while x != 0 or y != 0:
-              if x % 2 != y % 2:
-                  hamming_distance += 1
-              x = x >> 1
-              y = y >> 1
-          return hamming_distance
+    #Right way (Bitwise Operators): https://code.tutsplus.com/articles/understanding-bitwise-operators--active-11301
+    
+    # Approach 1: Just check every bit in both numbers and increment when they are different
+    def hammingDistance(self, x: int, y: int) -> int:
+        hamming_distance = 0
+        while x != 0 or y != 0:
+            if x % 2 != y % 2:
+                hamming_distance += 1
+            x = x >> 1
+            y = y >> 1
+        return hamming_distance
 
-      # Approach 2: Just make XOR of x and y and after that count the number of '1' bits.
-      # because XOR of two different bits is always 1
-      def hammingDistance(self, x: int, y: int) -> int:
-          hamming_distance = 0
-          new = x ^ y
-          while new > 0:
-              if new % 2 == 1:
-                  hamming_distance += 1
-              new = new >> 1
-          return hamming_distance
+    # Approach 2: Just make XOR of x and y and after that count the number of '1' bits.
+    # because XOR of two different bits is always 1
+    def hammingDistance(self, x: int, y: int) -> int:
+        hamming_distance = 0
+        new = x ^ y
+        while new > 0:
+            if new % 2 == 1:
+                hamming_distance += 1
+            new = new >> 1
+        return hamming_distance
 
-      # Approach 3: Again make XOR of x and y but when we count the number of '1' bits
-      # we make the trick n&(n-1) which removes last '1' bit
-      def hammingDistance(self, x: int, y: int) -> int:
-          hamming_distance = 0
-          new = x ^ y
-          while new > 0:
-              new = new & (new-1)
-              hamming_distance += 1
-          return hamming_distance
+    # Approach 3: Again make XOR of x and y but when we count the number of '1' bits
+    # we make the trick n&(n-1) which removes last '1' bit
+    def hammingDistance(self, x: int, y: int) -> int:
+        hamming_distance = 0
+        new = x ^ y
+        while new > 0:
+            new = new & (new-1)
+            hamming_distance += 1
+        return hamming_distance
 
-      May 5th Fix 
+    # Good explanation of XOR solution: https://www.youtube.com/watch?v=UP4GhCxeC4I
+
