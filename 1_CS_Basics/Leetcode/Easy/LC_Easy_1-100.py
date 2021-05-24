3812edc01f7c2d39301c674bd3d9fab7afe3e184
amichalo421 <andre.michalowski@gmail.com>
2021-05-24 08:59:43 -0400

    LCE1. Valid Phone Number (RegEx)

---
+++ 1_CS_Basics/Leetcode/Easy/LC_Easy_1-100.py
@@ -0,0 +1,10 @@
+1. VALID PHONE NUMBERS:
+  # Description - https://leetcode.com/problems/valid-phone-numbers/
+  # Discussion - https://leetcode.com/problems/valid-phone-numbers/discuss/?currentPage=1&orderBy=most_votes&query=
+  # Bash Script Overview - https://ryanstutorials.net/bash-scripting-tutorial/bash-script.php
+  # RegEx Reference - https://regexr.com/
+
+
+  # Short Version:
+    egrep "^(\([0-9]{3}\) |[0-9]{3}\-)[0-9]{3}\-[0-9]{4}$" file.txt
+
