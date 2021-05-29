477bc8a17d7beda0f49090cc1c9effe319e45924
amichalo421 <andre.michalowski@gmail.com>
2021-05-29 12:12:12 -0400

    (cw3. Solution rep)

--- 2_CS_Medium/CodeWars/CW_1.py
+++ 2_CS_Medium/CodeWars/CW_1.py
@@ -114,4 +114,12 @@
       # Append to new array
     # Return new array
 
-  
+  # Solution Repeat:
+    def unique_in_order(iterable):
+      pre_item = None
+      result = []
+      for item in iterable:
+          if item != pre_item:
+              result.append(item)
+              pre_item = item
+      return result
