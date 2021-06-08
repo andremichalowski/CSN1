c8119768df697860f8dae216ebfd4d1aad81cb42
amichalo421 <andre.michalowski@gmail.com>
2021-06-08 12:12:12 -0400

    5 - MAX SUBARR W/EXPL

--- 2_CS_Medium/Leetcode/Easy/LC_Easy_Arrays_1.py
+++ 2_CS_Medium/Leetcode/Easy/LC_Easy_Arrays_1.py
@@ -34,17 +34,25 @@ from _typeshed import OpenBinaryMode
 4. 35 - SEARCH INSERT POSITION (Given sorted arr and val, return index of val if found : index where it would be):
 
   def searchInsert(self, nums, target):
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
+    if target in nums:
+        return nums.index(target)
+    else:
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
 
 5. 53 - MAXIMUM SUBARRAY (Find max subarray in nums and return its sum):
 
-  
+  def maxSubArray(self, nums):
+    for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] += nums[i-1]
+    return max(nums)
 
+    # If the sum of a subarray is positive, it has possible to make the next value bigger, so we keep do it until it turn to negative.
+    # If the sum is negative, it has no use to the next element, so we break.
+    # it is a game of sum, not the elements.
+  
 6. 66 - PLUS ONE:
 
 7. 88 - MERGE SORTED ARRAY:
