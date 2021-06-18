d9e8d7d903af9613a8c4b9b4dd979981e6755ff3
amichalo421 <andre.michalowski@gmail.com>
2021-06-18 12:12:12 -0400

    2. Best time to buy and sell stock 2

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
@@ -1,4 +1,4 @@
-1. 121 - BEST TIME TO BUY AND SELL STOCK (Prices. Find best day to buy and then sell.):
+1. 121 - BEST TIME TO BUY AND SELL STOCK (Prices. Find the max profit buying on one day and selling on another.):
 
   def maxProfit(prices):
     max_profit, min_price = 0, float('inf')
@@ -8,3 +8,10 @@
         max_profit = max(max_profit, profit)
     return max_profit
 
+2. 122 - BEST TIME TO BUY AND SELL STOCK 2 (Find the max profit of buying and selling on different days in Prices arr.):
+
+  def maxProfit(self, prices: List[int]) -> int:
+    max_profit = 0
+    for i in range(len(prices) - 1):
+        max_profit += max(prices[i+1] - prices[i], 0)
+    return max_profit
