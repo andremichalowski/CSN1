5c1160967c8f202ad72de1ae41f7ab5ca8808833
amichalo421 <andre.michalowski@gmail.com>
2021-06-22 12:12:12 -0400

    8 - Count prime numbers under n

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
@@ -111,4 +111,15 @@
 
 7. 170 - TWO SUM III:
 
-8. 
+8. 204 - COUNT PRIMES (Count the number of prime numbers less than n):
+
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i * i, n, i):
+                    primes[j] = False
+        return sum(primes)
