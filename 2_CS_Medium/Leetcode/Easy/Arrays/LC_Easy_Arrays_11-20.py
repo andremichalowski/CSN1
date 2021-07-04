e5a25c6aa446c6856ea972e2a7802617e65a79be
amichalo421 <andre.michalowski@gmail.com>
2021-07-04 16:33:52 -0400

    6 - Majority Element (Candidate)

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
@@ -97,3 +97,14 @@
         m[n] = m.get(n, 0) + 1
         if m[n] > len(nums)//2:
             return n
+
+  def majorityElement(self, nums):
+    candidate, count = nums[0], 0
+    for num in nums:
+        if num == candidate:
+            count += 1
+        elif count == 0:
+            candidate, count = num, 1
+        else:
+            count -= 1
+    return candidate
