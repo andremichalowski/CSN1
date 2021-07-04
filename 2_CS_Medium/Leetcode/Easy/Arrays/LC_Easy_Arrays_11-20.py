31930ca0c4d642c774f58ef01f9e1041549915f9
amichalo421 <andre.michalowski@gmail.com>
2021-07-04 16:19:37 -0400

    5 - Two Sum II

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
@@ -58,3 +58,15 @@
         if target-num in dic:
             return [dic[target-num]+1, i+1]
         dic[num] = i
+  
+  # two-pointer
+  def twoSum1(self, numbers, target):
+    l, r = 0, len(numbers)-1
+    while l < r:
+        s = numbers[l] + numbers[r]
+        if s == target:
+            return [l+1, r+1]
+        elif s < target:
+            l += 1
+        else:
+            r -= 1
