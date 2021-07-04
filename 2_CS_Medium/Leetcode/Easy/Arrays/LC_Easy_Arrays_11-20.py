3a9b8c5e0f098a74f1ab15137c80aaf57b77c403
amichalo421 <andre.michalowski@gmail.com>
2021-07-04 16:32:30 -0400

    6 - Majority Element (Dict)

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_11-20.py
@@ -90,3 +90,10 @@
     def majorityElement1(self, nums):
         nums.sort()
         return nums[len(nums)//2]
+
+  def majorityElement2(self, nums):
+    m = {}
+    for n in nums:
+        m[n] = m.get(n, 0) + 1
+        if m[n] > len(nums)//2:
+            return n
