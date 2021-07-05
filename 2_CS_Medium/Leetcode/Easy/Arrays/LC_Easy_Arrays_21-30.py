f40eac95f1e02f16560b17fbaa326f71f968436a
amichalo421 <andre.michalowski@gmail.com>
2021-07-05 10:54:09 -0400

    21 - Summary Ranges (1)

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_21-30.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_21-30.py
@@ -1,4 +1,13 @@
-21. 228 - SUMMARY RANGES:
+21. 228 - SUMMARY RANGES (Find the smallest set of ranges that summarize the ints in arr):
+
+  # Collect the ranges then format and return them
+  def summaryRanges(self, nums):
+    ranges = []
+    for n in nums:
+        if not ranges or n > ranges[-1][-1] + 1:
+            ranges += [],
+        ranges[-1][1:] = n,
+    return ['->'.join(map(str, r)) for r in ranges]
 
 22. 243 - SHORTEST WORD DISTANCE:
 
