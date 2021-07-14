60745ce11a921cc3e7761d8add6c9f7460959f3e
amichalo421 <andre.michalowski@gmail.com>
2021-07-13 22:59:10 -0400

    Rebasing

---
+++ Rebasing_Tests/Rebasing.js
@@ -0,0 +1,25 @@
+Test commits:
+
+Test 1
+
+Test 2
+
+Test 3
+
+Test 4
+
+Test 5
+
+Test 6
+
+Test 7
+
+Test 8
+
+New Branch Merge Test 
+
+Back to main after feature commits were made
+
+NEW TEST 7 FEATURE BRANCH
+
+NEW TEST 8 FEATURE BRANCH
