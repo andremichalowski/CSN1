83e7e6ebcebfcd0a4c2f2224ab189285a96ad3ba
amichalo421 <andre.michalowski@gmail.com>
2021-07-14 21:02:23 -0400

    2. Find all numbers dissapearing

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
@@ -1,6 +1,21 @@
-1. "422 - VALID WORD SQUARE"
+1. "422 - VALID WORD SQUARE Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums."
+
+
 
 2. "448 - FIND ALL NUMBERS DISSAPPEARING"
+
+  def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    for i in range(len(nums)):
+        temp = abs(nums[i]) - 1
+        if nums[temp] > 0:
+            nums[temp] *= -1
+    
+    res = []
+    for i,n in enumerate(nums):
+        if n > 0:
+            res.append(i+1)
+      
+    return res
 
 3. "453 - MINIMUM MOVES TO EQUAL"
 
