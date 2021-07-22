93b5bc141cee907c218c75b00d07787eb40acdf7
amichalo421 <andre.michalowski@gmail.com>
2021-07-21 20:24:21 -0400

    9. (Question)

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
@@ -121,8 +121,17 @@
     # the second row consists of the characters "asdfghjkl", and
     # the third row consists of the characters "zxcvbnm".
 
-    Input: words = ["Hello","Alaska","Dad","Peace"]
-    Output: ["Alaska","Dad"]
+    # Input: words = ["Hello","Alaska","Dad","Peace"]
+    # Output: ["Alaska","Dad"]
+
+    def findWords(self, words):
+        line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+        ret = []
+        for word in words:
+            w = set(word.lower())
+            if w <= line1 or w <= line2 or w <= line3:
+                ret.append(word)
+        return ret
 
 
 
