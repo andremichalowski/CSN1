d806e33c89e88f767453b1755c292fba2e7ddb26
amichalo421 <andre.michalowski@gmail.com>
2021-07-21 20:09:35 -0400

    9. 500 - Keyboard Row: Question

--- 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
+++ 2_CS_Medium/Leetcode/Easy/Arrays/LC_Easy_Arrays_31-40.py
@@ -113,4 +113,14 @@
 
 9. "500 - KEYBOARD ROW"
 
+    # Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+    # In the American keyboard:
+
+    # the first row consists of the characters "qwertyuiop",
+    # the second row consists of the characters "asdfghjkl", and
+    # the third row consists of the characters "zxcvbnm".
+
+
+
 10. "506 - RELATIVE RANKS"
